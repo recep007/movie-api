cb77a843ae621f8cde8ca4d61e621795e41e8286
recep007 <recepyilancioglu007@gmail.com>
2018-07-05 23:25:42 +0300

    movie-api final commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index');
 const movie = require('./routes/movie');
+const director = require('./routes/director');
 
 const app = express();
 //db connection
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/movies', movie);
+app.use('/api/directors', director);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- models/Movie.js
+++ models/Movie.js
@@ -2,14 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
  const MovieSchema = new Schema({
-   director_id: Schema.Types.ObjectId,
+   director_id: {
+     type: Schema.Types.ObjectId,
+     required: true
+   },
    title: {
      type: String,
      required: true
    },
    category: String,
    country: String,
-   year: Number,
+   year: {
+     type: Number,
+     max: 2019,
+     min: 1900
+   },
    imdb_score: Number,
    createDate: {
      type:Date,
